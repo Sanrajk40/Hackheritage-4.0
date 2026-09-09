4770ae04732eb6ffe360382bd3fc68f735967b34
Sanraj Kumar <kumarsanraj40@gmail.com>
2026-09-09 17:07:22 +0530

    Pushed something

--- backend/globalapp/admin.py
+++ backend/globalapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import globe_employee
+from .models import globe_employee,DEL_pass
 from django.contrib.auth.models import User,Group
 # Register your models here.
 
 admin.site.register(globe_employee)
 admin.site.unregister(User)
 admin.site.unregister(Group)
+admin.site.register(DEL_pass)
